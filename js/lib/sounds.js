b3db6cc62bd789305265e1217f693c1366bcd747
Sam Greenlee <sam.a.greenlee@gmail.com>
2016-06-18 18:01:22 -0400

    Change URL for mp3s

--- js/lib/sounds.js
+++ js/lib/sounds.js
@@ -1,9 +1,9 @@
 var _musicEndCallback = function () {};
 
 var Sounds = {
-  music: new Audio("/assets/mp3/bach-minuet-g-minor.mp3"),
-  boo: new Audio("/assets/mp3/boo.mp3"),
-  applause: new Audio("/assets/mp3/applause.mp3"),
+  music: new Audio("https://raw.githubusercontent.com/sgreenlee/keyboard-hero/assets/mp3/bach-minuet-g-minor.mp3"),
+  boo: new Audio("https://raw.githubusercontent.com/sgreenlee/keyboard-hero/assets/mp3/boo.mp3"),
+  applause: new Audio("https://raw.githubusercontent.com/sgreenlee/keyboard-hero/assets/mp3/applause.mp3"),
   setMusicEndCallback: function (callback) {
     _musicEndCallback = callback;
   }
