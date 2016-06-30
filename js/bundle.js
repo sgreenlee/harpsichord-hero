8bd217346ffaa064f2dcc4d620a7d84adb0ed4b1
Sam Greenlee <sam.a.greenlee@gmail.com>
2016-06-29 21:52:04 -0400

    bundle

--- js/bundle.js
+++ js/bundle.js
@@ -46,14 +46,14 @@
 
 	// var synth = require("./lib/synth/synth.js");
 
-	var maps = __webpack_require__(5);
+	var maps = __webpack_require__(1);
 	var KEY_MAPS = maps.KEY_MAPS;
 	var KEY_NOTE_MAP = maps.KEY_NOTE_MAP;
 
-	window.Game = __webpack_require__(6);
-	window.StarMeter = __webpack_require__(9);
-	window.Sounds = __webpack_require__(10);
-	window.Modals = __webpack_require__(11);
+	window.Game = __webpack_require__(9);
+	window.StarMeter = __webpack_require__(6);
+	window.Sounds = __webpack_require__(5);
+	window.Modals = __webpack_require__(8);
 	// require("./lib/midi.js");
 
 	function onWin () {
@@ -109,11 +109,7 @@
 
 
 /***/ },
-/* 1 */,
-/* 2 */,
-/* 3 */,
-/* 4 */,
-/* 5 */
+/* 1 */
 /***/ function(module, exports) {
 
 	var keyMaps = {
@@ -148,14 +144,265 @@
 
 
 /***/ },
-/* 6 */
+/* 2 */,
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var maps = __webpack_require__(5);
+	var NOTE_NAMES = __webpack_require__(1).NOTE_NAMES;
+	var NUM_NOTES = NOTE_NAMES.length;
+
+	function Note (noteName, time) {
+	  this.name = noteName;
+	  this.time = time;
+	}
+
+	Note.prototype.generateDOMElement = function() {
+	  this.element = $("<div>").addClass("note").addClass(this.name);
+	};
+
+	Note.random = function () {
+	  var noteName = NOTE_NAMES[Math.floor(Math.random() * NUM_NOTES)];
+	  return new Note(noteName);
+	};
+
+	module.exports = Note;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	function Node (item, prev, next) {
+	  this.item = item;
+	  this.prev = prev || null;
+	  this.next = next || null;
+	}
+
+	Node.prototype.delete = function () {
+	  this.prev.next = this.next;
+	  this.next.prev = this.prev;
+	};
+
+	function LinkedList () {
+	  this._head = new Node(null);
+	  this._tail = new Node(null);
+	  this._tail.prev = this._head;
+	  this._head.next = this._tail;
+	}
+
+	LinkedList.prototype.add = function (item) {
+	  var node = new Node(item);
+	  node.prev = this._tail.prev;
+	  node.next = this._tail;
+	  this._tail.prev.next = node;
+	  this._tail.prev = node;
+	};
+
+	LinkedList.prototype.forEach = function(callback) {
+	  var argsArray = [].slice.call(arguments, 1);
+	  var currentNode = this._head.next;
+	  while (currentNode.next !== null) {
+	    callback.apply(null, [currentNode].concat(argsArray));
+	    currentNode = currentNode.next;
+	  }
+	};
+
+	LinkedList.prototype.while = function (truthyFunc, callback) {
+	  var argsArray = [].slice.call(arguments, 2);
+	  var currentNode = this._head.next;
+	  while (currentNode.next !== null && truthyFunc(currentNode)) {
+	    callback.apply(null, [currentNode].concat(argsArray));
+	    currentNode = currentNode.next;
+	  }
+	};
+
+	module.exports = LinkedList;
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	var _musicEndCallback = function () {};
+
+	var Sounds = {
+	  music: new Audio("https://s3.amazonaws.com/hhero-pro/bach_minuet_g_major.mp3"),
+	  boo: new Audio("https://s3.amazonaws.com/hhero-pro/boo.mp3"),
+	  applause: new Audio("https://s3.amazonaws.com/hhero-pro/applause.mp3"),
+	  setMusicEndCallback: function (callback) {
+	    _musicEndCallback = callback;
+	  }
+	};
+
+	Sounds.music.addEventListener("ended", function () {
+	  _musicEndCallback();
+	});
+
+	module.exports = Sounds;
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	var $starMeter = $('#star-meter');
+	var $starMeterHP = $('#star-meter-hp');
+
+	var _hp = 60;
+	var _loseCallback = function () {};
+
+	function _calculateMeterColor () {
+	  var green = Math.floor((217 - 22) * (_hp / 100) + 22);
+	  var red = Math.floor(239 - green);
+	  return "rgb(" + red +", " + green + ", 41)";
+	}
+
+	function updateMeter () {
+	  $starMeterHP.css("height", _hp + "%");
+	  $starMeterHP.css("background-color", _calculateMeterColor());
+	}
+
+	var starMeter = {
+	  reset: function () {
+	    _hp = 60;
+	    updateMeter();
+	  },
+
+	  bonus: function (timeDelta) {
+	    var score = (100 - Math.abs(timeDelta)) / 10;
+	    _hp += score;
+	    if (_hp > 100) {
+	      _hp = 100;
+	    }
+	    updateMeter();
+	  },
+
+	  malus: function (score) {
+	    _hp -= score;
+	    updateMeter();
+	    if (this.isLost()) {
+	      _loseCallback();
+	    }
+	  },
+
+	  isLost: function () {
+	    return _hp < 20;
+	  },
+
+	  setLoseCallback: function (callback) {
+	    _loseCallback = callback;
+	  }
+	};
+
+	updateMeter();
+
+	module.exports = starMeter;
+
+
+/***/ },
+/* 7 */,
+/* 8 */
+/***/ function(module, exports) {
+
+	var $modalOverlay = $("#modal-overlay");
+	var $loseModal = $(".modal.lose");
+	var $winModal = $(".modal.win");
+	var $loadModal = $(".modal.load");
+	var $body = $("body");
+
+	var _restartCallback = function () {};
+
+	var newGameButtonList = document.querySelectorAll(".new-game");
+
+	function restart () {
+	  _restartCallback();
+	}
+
+	for (var i = 0; i < newGameButtonList.length; i++) {
+	  newGameButtonList[i].addEventListener("click", restart);
+	}
+
+	$modalOverlay.detach();
+	$modalOverlay.css("display", "block");
+
+	$loseModal.detach();
+	$loseModal.css("top", "50%");
+
+	$winModal.detach();
+	$winModal.css("top", "50%");
+
+	$loadModal.detach();
+	$loadModal.css("top", "50%");
+
+	var currentModal = null;
+
+	function closeModals () {
+	  modalGroup.removeClass("active");
+	}
+
+	function openLoseModal () {
+	  loseModalGroup.addClass("active");
+	}
+
+	var loseModal = {
+	  open: function () {
+	    $body.append($modalOverlay);
+	    $body.append($loseModal);
+	    currentModal = this;
+	  },
+	  close: function () {
+	    $modalOverlay.detach();
+	    $loseModal.detach();
+	  }
+	};
+
+	var winModal = {
+	  open: function () {
+	    $body.append($modalOverlay);
+	    $body.append($winModal);
+	    currentModal = this;
+	  },
+	  close: function () {
+	    $modalOverlay.detach();
+	    $winModal.detach();
+	  }
+	};
+
+	var loadModal = {
+	  open: function () {
+	    $body.append($modalOverlay);
+	    $body.append($loadModal);
+	    currentModal = this;
+	  },
+	  close: function () {
+	    $modalOverlay.detach();
+	    $loadModal.detach();
+	  }
+	};
+
+	module.exports = {
+	  setRestartCallback: function (callback) {
+	    _restartCallback = callback;
+	  },
+
+	  lose: loseModal,
+	  win: winModal,
+	  load: loadModal,
+	  close: function () {
+	    currentModal.close();
+	  }
+	};
+
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var maps = __webpack_require__(1);
 	var KEY_MAPS = maps.KEY_MAPS;
 	var KEY_NOTE_MAP = maps.KEY_NOTE_MAP;
-	var Note = __webpack_require__(7);
-	var LinkedList = __webpack_require__(8);
+	var Note = __webpack_require__(3);
+	var LinkedList = __webpack_require__(4);
 
 	var DISTANCE_TO_TRAVEL = 13000;
 	var SECONDS_TO_TRAVEL = 11;
@@ -295,255 +542,6 @@
 	module.exports = {
 	  load: load,
 	  stop: stop
-	};
-
-
-/***/ },
-/* 7 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var NOTE_NAMES = __webpack_require__(5).NOTE_NAMES;
-	var NUM_NOTES = NOTE_NAMES.length;
-
-	function Note (noteName, time) {
-	  this.name = noteName;
-	  this.time = time;
-	}
-
-	Note.prototype.generateDOMElement = function() {
-	  this.element = $("<div>").addClass("note").addClass(this.name);
-	};
-
-	Note.random = function () {
-	  var noteName = NOTE_NAMES[Math.floor(Math.random() * NUM_NOTES)];
-	  return new Note(noteName);
-	};
-
-	module.exports = Note;
-
-
-/***/ },
-/* 8 */
-/***/ function(module, exports) {
-
-	function Node (item, prev, next) {
-	  this.item = item;
-	  this.prev = prev || null;
-	  this.next = next || null;
-	}
-
-	Node.prototype.delete = function () {
-	  this.prev.next = this.next;
-	  this.next.prev = this.prev;
-	};
-
-	function LinkedList () {
-	  this._head = new Node(null);
-	  this._tail = new Node(null);
-	  this._tail.prev = this._head;
-	  this._head.next = this._tail;
-	}
-
-	LinkedList.prototype.add = function (item) {
-	  var node = new Node(item);
-	  node.prev = this._tail.prev;
-	  node.next = this._tail;
-	  this._tail.prev.next = node;
-	  this._tail.prev = node;
-	};
-
-	LinkedList.prototype.forEach = function(callback) {
-	  var argsArray = [].slice.call(arguments, 1);
-	  var currentNode = this._head.next;
-	  while (currentNode.next !== null) {
-	    callback.apply(null, [currentNode].concat(argsArray));
-	    currentNode = currentNode.next;
-	  }
-	};
-
-	LinkedList.prototype.while = function (truthyFunc, callback) {
-	  var argsArray = [].slice.call(arguments, 2);
-	  var currentNode = this._head.next;
-	  while (currentNode.next !== null && truthyFunc(currentNode)) {
-	    callback.apply(null, [currentNode].concat(argsArray));
-	    currentNode = currentNode.next;
-	  }
-	};
-
-	module.exports = LinkedList;
-
-
-/***/ },
-/* 9 */
-/***/ function(module, exports) {
-
-	var $starMeter = $('#star-meter');
-	var $starMeterHP = $('#star-meter-hp');
-
-	var _hp = 60;
-	var _loseCallback = function () {};
-
-	function _calculateMeterColor () {
-	  var green = Math.floor((217 - 22) * (_hp / 100) + 22);
-	  var red = Math.floor(239 - green);
-	  return "rgb(" + red +", " + green + ", 41)";
-	}
-
-	function updateMeter () {
-	  $starMeterHP.css("height", _hp + "%");
-	  $starMeterHP.css("background-color", _calculateMeterColor());
-	}
-
-	var starMeter = {
-	  reset: function () {
-	    _hp = 60;
-	    updateMeter();
-	  },
-
-	  bonus: function (timeDelta) {
-	    var score = (100 - Math.abs(timeDelta)) / 10;
-	    _hp += score;
-	    if (_hp > 100) {
-	      _hp = 100;
-	    }
-	    updateMeter();
-	  },
-
-	  malus: function (score) {
-	    _hp -= score;
-	    updateMeter();
-	    if (this.isLost()) {
-	      _loseCallback();
-	    }
-	  },
-
-	  isLost: function () {
-	    return _hp < 20;
-	  },
-
-	  setLoseCallback: function (callback) {
-	    _loseCallback = callback;
-	  }
-	};
-
-	updateMeter();
-
-	module.exports = starMeter;
-
-
-/***/ },
-/* 10 */
-/***/ function(module, exports) {
-
-	var _musicEndCallback = function () {};
-
-	var Sounds = {
-	  music: new Audio("https://s3.amazonaws.com/hhero-pro/bach_minuet_g_major.mp3"),
-	  boo: new Audio("https://s3.amazonaws.com/hhero-pro/boo.mp3"),
-	  applause: new Audio("https://s3.amazonaws.com/hhero-pro/applause.mp3"),
-	  setMusicEndCallback: function (callback) {
-	    _musicEndCallback = callback;
-	  }
-	};
-
-	Sounds.music.addEventListener("ended", function () {
-	  _musicEndCallback();
-	});
-
-	module.exports = Sounds;
-
-
-/***/ },
-/* 11 */
-/***/ function(module, exports) {
-
-	var $modalOverlay = $("#modal-overlay");
-	var $loseModal = $(".modal.lose");
-	var $winModal = $(".modal.win");
-	var $loadModal = $(".modal.load");
-	var $body = $("body");
-
-	var _restartCallback = function () {};
-
-	var newGameButtonList = document.querySelectorAll(".new-game");
-
-	function restart () {
-	  _restartCallback();
-	}
-
-	for (var i = 0; i < newGameButtonList.length; i++) {
-	  newGameButtonList[i].addEventListener("click", restart);
-	}
-
-	$modalOverlay.detach();
-	$modalOverlay.css("display", "block");
-
-	$loseModal.detach();
-	$loseModal.css("top", "50%");
-
-	$winModal.detach();
-	$winModal.css("top", "50%");
-
-	$loadModal.detach();
-	$loadModal.css("top", "50%");
-
-	var currentModal = null;
-
-	function closeModals () {
-	  modalGroup.removeClass("active");
-	}
-
-	function openLoseModal () {
-	  loseModalGroup.addClass("active");
-	}
-
-	var loseModal = {
-	  open: function () {
-	    $body.append($modalOverlay);
-	    $body.append($loseModal);
-	    currentModal = this;
-	  },
-	  close: function () {
-	    $modalOverlay.detach();
-	    $loseModal.detach();
-	  }
-	};
-
-	var winModal = {
-	  open: function () {
-	    $body.append($modalOverlay);
-	    $body.append($winModal);
-	    currentModal = this;
-	  },
-	  close: function () {
-	    $modalOverlay.detach();
-	    $winModal.detach();
-	  }
-	};
-
-	var loadModal = {
-	  open: function () {
-	    $body.append($modalOverlay);
-	    $body.append($loadModal);
-	    currentModal = this;
-	  },
-	  close: function () {
-	    $modalOverlay.detach();
-	    $loadModal.detach();
-	  }
-	};
-
-	module.exports = {
-	  setRestartCallback: function (callback) {
-	    _restartCallback = callback;
-	  },
-
-	  lose: loseModal,
-	  win: winModal,
-	  load: loadModal,
-	  close: function () {
-	    currentModal.close();
-	  }
 	};
 
 
